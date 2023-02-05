97cd692fa33ba6b7ee1c22772f51e8454ed58025
Sajad Alimoradi <sajad.alimoradi@outlook.com>
2023-02-06 00:08:27 +0330

    fix(comment): uncomment banner function
    - in the previous commit banner was comment now it's work

--- dragonV5.py
+++ dragonV5.py
@@ -58,7 +58,7 @@ for example: login/register/level/establishing roles of game, etc
 '''
 
 # Banner is running then user's register or login will be run
-# banner.banner()
+banner.banner()
 while True:
     clear_screen()
     start_message = Figlet(font='standard', width=110)
